7b6f88f27798b4d7658c47fbe180516c7358e753
piyushnagre <142238525+piyushnagre@users.noreply.github.com>
2023-09-06 03:50:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
-const populate = async (value, currency) => {
+const populate = async (amount, currency) => {
+    
     let myStr = ""
-    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_vom9BA2r2KSNRdrMgmldlSMuPyOyiCNyaBgN4QFV&base_currency=" + currency
+
+    //modify url "&base_currency=" + currency
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_e9tVEpkt644lqD8Q0YcpKrONFUwug5MBYac6qyvL&base_currency=" + currency
+
     let response = await fetch(url)
-    let rJson = await response.json()
+    let input = await response.json()
+
     document.querySelector(".output").style.display = "block"       //to make table visible
 
-    for (let key of Object.keys(rJson["data"])) {
+    //the 'input' response contains various "data"-"value" mapping for different currencies 
+    for (let key of Object.keys(input["data"])) {
 
         //we use `` to convert the json format into string format.
         myStr += ` <tr>
                         <td>${key}</td>
-                        <td>${parseFloat((rJson["data"][key]["value"] * value).toFixed(2))}</td>
+                        <td>${parseFloat((input["data"][key]["value"] * amount).toFixed(2))}</td>
                     </tr> 
                 `
     }
+
+    //inserting converted currencies in the table
     const tableBody = document.querySelector("tbody");
     tableBody.innerHTML = myStr;
 }
+
 const btn = document.querySelector(".btn")
 btn.addEventListener("click", (e) => {
+
     e.preventDefault()
-    const value = parseInt(document.querySelector("input[name='quantity']").value);
+    const amount = parseInt(document.querySelector("input[name='quantity']").value);
     const currency = document.querySelector("select[name='currency']").value
-    populate(value, currency)
+    populate(amount, currency)
 })
